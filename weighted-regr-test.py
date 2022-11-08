eca228b48b7ffe62e0f6de3500facc8c1f038f32
Ayoub Zinini <ayoub.zinini@usmba.ac.ma>
2022-11-08 09:55:03 +0100

    update

---
+++ weighted-regr-test.py
@@ -0,0 +1,53 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import KFold,cross_val_predict, LeaveOneOut
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import mean_squared_error, mean_squared_log_error, max_error, r2_score, mean_absolute_error
+from sklearn.model_selection import train_test_split, cross_val_score
+import seaborn as sns
+from matplotlib.pyplot import show, bar, axhline
+from pandas import read_csv, read_excel
+import matplotlib.pyplot as plt
+from pandas import DataFrame, concat
+import numpy as np
+from scipy.stats import f_oneway
+from pca import pca
+file_name=input('File Name : ')
+db=read_excel(file_name+'.xlsx')
+X=db.drop([db.columns[0],'Y'],axis=1)
+Y=db['Y']
+def simple_moving_average(signal, window=5):
+    return np.convolve(signal, np.ones(window)/window, mode='same')
+r=DataFrame({"one":np.ones(X.shape[1])})
+for i in X.index:
+  r=concat([r,DataFrame({str(i):simple_moving_average(X.loc[i,], window=5)})],axis=1)
+X=r.T.drop("one",axis=0)
+i=0
+while True:
+  model = pca(n_components=20)
+  results = model.fit_transform(X)
+  x_train, x_test, y_train, y_test = train_test_split(results['PC'],Y,test_size=0.2,random_state=i)
+  income_groups=[y_train,y_test]
+  s,p=f_oneway(*income_groups)
+  wregr=LinearRegression()
+  wregr.fit(x_train, y_train, np.ones(x_train.shape[0])/np.mean(x_train,axis=1)**2)
+  Y_cv = cross_val_predict(wregr, x_train, y_train, cv=LeaveOneOut())
+  score_cv = r2_score(y_train, Y_cv)
+  mse_cv = mean_squared_error(y_train, Y_cv)
+  mse_train=mean_squared_error(y_train, wregr.predict(x_train))
+  mse_test=mean_squared_error(y_test, wregr.predict(x_test))
+  score_test=r2_score(y_test,wregr.predict(x_test))
+  score_train=r2_score(y_train,wregr.predict(x_train))
+  i=i+1
+  if p<0.05 and score_test>0 and score_cv>0:
+    break
+print('R2 CV (wregr): ',100 * score_cv," %")
+print('RMSE CV (wregr): ',np.sqrt(mse_cv))
+print('R2 calibration: ',100 * score_train," %")
+print('RMSE calibration: ',np.sqrt(mse_train))
+print('R2 test: ',100 * score_test," %")
+print('RMSE test: ',np.sqrt(mse_test))
+print("Best random state : ",i)
+perc_coefs=DataFrame({'idx':results['PC'].columns,'val':[abs(i)/np.sum(abs(wregr.coef_)) for i in wregr.coef_]}).sort_values(by=['val'])
+bar(perc_coefs['idx'],perc_coefs['val'])
+axhline(y=0.8,linewidth=1, color='red')
+show()
