bd0b3424b0f28be5090f9a9cc6d60db2f0ef4c6b
Ayoub Zinini <62222080+Ayoubzinini@users.noreply.github.com>
2024-05-16 10:18:32 +0100

    update

---
+++ gpr-test.py
@@ -0,0 +1,25 @@
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from pandas import read_excel ,DataFrame
+
+# Load NIR reflectance data (X) and oil acidity data (y) 
+db=read_excel("data-oil-no-mirrors.xlsx")#data-oil-12+31janv 
+X=db.drop(['Unnamed: 0','Y'],axis=1)
+Y=db['Y']
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+
+# Initialize GPR model with RBF kernel
+gpr = GaussianProcessRegressor(kernel=None, n_restarts_optimizer=10)
+
+# Train model
+gpr.fit(X_train, y_train)
+
+# Make predictions
+y_pred = gpr.predict(X_test)
+
+# Evaluate model
+mse = mean_squared_error(y_test, y_pred)
+print("MSE:", mse)
+print("R2 : ", r2_score(y_test, y_pred))
